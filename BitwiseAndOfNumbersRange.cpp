fe4ecd168e6b4f49a2be2f2871ffb9a026d9c2ae
zrss <huangzhesi@gmail.com>
2015-04-27 09:48:21 +0800

    Leetcode 201

--- BitwiseAndOfNumbersRange.cpp
+++ BitwiseAndOfNumbersRange.cpp
@@ -1,6 +1,29 @@
+/* Leetcode 201 one try AC 105MS */
+
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
+      if (m == n)
+        return m;
 
+      int g = m & n, rel = g;
+      int mask = ~0 << 1;
+      int pow = 1;
+
+      while (g) {
+        int b = g & 1;
+        if (b) {
+          int temp = n & mask;
+
+          if (temp >= m) {
+            rel &= ~pow;
+          }
+        }
+        mask <<= 1;
+        pow <<= 1;
+        g >>= 1;
+      }
+
+      return rel;
     }
 };
