74dd19426265d26e5c30df14544031dbf88edded
zrss <huangzhesi@gmail.com>
2016-01-25 22:45:39 +0800

    thinking

--- MaxPointsonaLine.cpp
+++ MaxPointsonaLine.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
@@ -11,35 +11,64 @@ struct Point {
     Point(int a, int b) : x(a), y(b) {}
 };
 
-bool cmp(Point& pA, Point& pB) {
-	if (pA.x != pB.x) {
-		return pA.x < pB.x;
-	}
-	return pA.y < pB.y;
-}
-
 class Solution {
 public:
     int maxPoints(vector<Point>& points) {
-        if (points.size() < 2) {
-        	return 0;
-        }
-        if (points.size() == 2) {
-        	return 1;
+        if (points.size() == 0) {
+            return 0;
         }
 
-        sort(points.begin(), points.end(), cmp);
+        int max = 0;
 
-        
-    }
+        for (int i = 0; i < points.size(); ++i) {
+            unordered_map<double, int> map;
 
-    bool inLine(Point& p1, Point& p2, Point& p3) {
-    	return (p1.y - p2.y) * (p3.x - p2.x) == (p1.x - p2.x) * (p3.y - p2.y);
+            int sameCount = 1;
+            int infinite = 0;
+
+            for (int j = i + 1; j < points.size(); ++j) {
+                if (points[i].x == points[j].x && points[i].y == points[j].y) {
+                    ++sameCount;
+                } else if (points[i].x == points[j].x) {
+                    ++infinite;
+                } else {
+                    double k = (double)(points[i].y - points[j].y) / (points[i].x - points[j].x); // ps: (double)
+                    ++map[k];
+                }
+            }
+
+            int tmp = sameCount + infinite;
+            if (tmp > max) {
+                max = tmp;
+            }
+
+            for (auto itr = map.begin(); itr != map.end(); ++itr) {
+                int cur = (itr->second) + sameCount;
+                if (cur > max) {
+                    max = cur;
+                }
+            }
+        }
+
+        return max;
     }
 };
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
 	Solution solution;
+
+    Point p1(84, 250);
+    Point p2(0, 0);
+    Point p3(1, 0);
+    Point p4(0, -70);
+    Point p5(0, -70);
+    Point p6(1, -1);
+    Point p7(21, 10);
+    Point p8(42, 90);
+    Point p9(-42, -230);
+
+    vector<Point> points = {p1, p2, p3, p4, p5, p6, p7, p8, p9};
+    cout << solution.maxPoints(points) << endl;
+
 	return 0;
 }
