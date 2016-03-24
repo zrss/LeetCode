ea6f6901e2ca2648121d9c613a8ab0f76e130b56
zrss <huangzhesi@gmail.com>
2016-03-24 08:57:34 +0800

    the game of thinking

--- FindTheDuplicateNumber.cpp
+++ FindTheDuplicateNumber.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// 思路1：暴力枚举，尝试遍历1-n个数，每次遍历n+1遍
+// 思路2：鸽巢原理，假设1-n区间内，比n/2大的数，还大于n/2，则重复的数字处于n/2 - n 之间，反之则处于1 - n/2之间
 class Solution {
 public:
 	int findDuplicate(vector<int>& nums) {
