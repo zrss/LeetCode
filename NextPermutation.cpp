6bd69d7720e29a195ee4f607c81fe4e5faf7509a
zrss <huangzhesi@gmail.com>
2016-03-31 22:41:22 +0800

    the game of thinking

--- NextPermutation.cpp
+++ NextPermutation.cpp
@@ -4,51 +4,33 @@
 
 using namespace std;
 
+// 观察
+// [1, 2, 3, 4, 5]
+// [1, 2, 3, (5, 4)]
+// [1, 2, (4, 3, 5)]
+
+// 通过观察发现，从右至左找顺序对，找到之后交换，并把后续元素排序，即是下一个排列
+
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-    	// is reverse ?
-    	bool reverse = true;
-
-    	for (int i = nums.size() - 1; reverse && i > 0; --i) {
-    		if (nums[i - 1] < nums[i]) {
-    			reverse = false;
+    	// 
+    	for (int i = nums.size() - 1; i >= 0; --i) {
+    		for (int j = nums.size() - 1; j > i; --j) {
+    			if (nums[i] < nums[j]) {
+    				swap(nums[i], nums[j]);
+    				sort(nums.begin() + i + 1, nums.end());
+    				return;
+    			}
     		}
     	}
-
-    	if (!reverse) {
-	    	int maxIndex = -1;
-    		int startIndex = nums.size() - 1;
-
-    		for (int i = startIndex; i > maxIndex; --i) {
-	        	for (int j = i - 1; j >= 0; --j) {
-	        		if (nums[j] < nums[i]) {
-	        			if (j > maxIndex) {
-	        				maxIndex = j;
-	        				startIndex = i;
-	        			} else if (j == maxIndex) {
-	        				if (nums[startIndex] > nums[i]) {
-	        					startIndex = i;
-	        				}
-	        			}
-	        			break;
-	        		}
-		        }
-	        }
-
-	        swap(nums[maxIndex], nums[startIndex]);
-
-	        sort(nums.begin() + maxIndex + 1, nums.end());
-
-    	} else {
-        	sort(nums.begin(), nums.end());
-    	}
+		sort(nums.begin(), nums.end());
     }
 };
 
 int main(int argc, char const *argv[]) {
 	Solution solution;
-	vector<int> nums = {2,3,1};
+	vector<int> nums = {1,2,3};
 	solution.nextPermutation(nums);
 
 	for (auto itr = nums.begin(); itr != nums.end(); ++itr) {
