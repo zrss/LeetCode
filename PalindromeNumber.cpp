1f032b0c60726664e509cffb2db3d168774335fd
zrss <huangzhesi@gmail.com>
2016-04-23 17:11:37 +0800

    the game of thinking

--- PalindromeNumber.cpp
+++ PalindromeNumber.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
+// 方法1
+// 直接提取每一位数字
+// 即使有溢出也能正确返回false
+//
+// 方法2
+// 用数组将每位数字存储起来再做比较
+
 class Solution {
 public:
     bool isPalindrome(int x) {
     	if (x < 0) {
     	    return false;
     	}
-        
+
         int value = 0;
         int tmp = x;
 
