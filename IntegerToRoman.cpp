f5c69c2ec053328e8ecf6e07e8d700fdef5e0066
zrss <huangzhesi@gmail.com>
2016-04-24 10:48:13 +0800

    减法游戏

--- IntegerToRoman.cpp
+++ IntegerToRoman.cpp
@@ -3,11 +3,13 @@
 
 using namespace std;
 
+// 减法游戏
+
 class Solution {
 public:
     string intToRoman(int num) {
-        string str[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string str[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}; // 相当于十三位
+        int value[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}; // 相应数值
 
         string rel;
         if (num <= 0) {
@@ -15,6 +17,8 @@ public:
         }
         
         for (int i = 0; i < 13; ++i) {
+            // 如果大于最高位的最大值
+            // 则持续减
         	while (num >= value[i]) {
         		num -= value[i];
         		rel += str[i];
