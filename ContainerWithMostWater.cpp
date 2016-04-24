96e561481f9b7cabe1c6a9a7a24b8221069a1e1d
zrss <huangzhesi@gmail.com>
2016-04-24 10:42:51 +0800

    the game of thinking

--- ContainerWithMostWater.cpp
+++ ContainerWithMostWater.cpp
@@ -3,31 +3,36 @@
 
 using namespace std;
 
+// 方法1
+// 简单粗暴
+// 枚举[i, j]组合
+// O(N^2)
+
+// 方法2
+// 木桶原理，很高大上对吧
+// 其实吧是因为[i, j]组合能容纳的水的多少和min(h[i], h[j])有关
+// 即只与短的木板有关系
+
+// 所以只需移动短板就可以找到最大的蓄水量了
+// 双指针
+// l = 0, r = len - 1
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
     	int l = 0, r = height.size() - 1;
-        int max = 0;
+        int rel = 0;
 
         while (l < r) {
+            rel = max(rel, min(height[l], height[r]) * (r - l));
             if (height[l] < height[r]) {
-                int tmp = height[l] * (r - l);
-                if (tmp > max) {
-                    max = tmp;
-                }
-                int low = height[l];
-                while (l < r && height[++l] <= low);
+                ++l;
             } else {
-                int tmp = height[r] * (r - l);
-                if (tmp > max) {
-                    max = tmp;
-                }
-                int low = height[r];
-                while (l < r && height[--r] <= low);
+                --r;
             }
         }
 
-        return max;
+        return rel;
     }
 };
 
