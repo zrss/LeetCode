44b4ef5e4275074afd7778a7ba89b79a9c4fd285
huangzhesi <123456>
2016-07-12 14:24:34 +0800

    young coder hzs just want to fight like anyone else

--- WaterAndJugProblem.cpp
+++ WaterAndJugProblem.cpp
@@ -3,6 +3,35 @@
 
 using namespace std;
 
+// what a hard problem -_-
+// kill my whole morning spare time
+// I spend so much time hand write that transform
+// finally get a regular for programm to implement it
+// abstract some denotes from problem
+
+// target z, num b1, num b2
+
+// assert that b1 < b2
+
+// then do following operate
+// pour b1 to b2
+// refill b1 then pour b1 to b2
+// that remain in b1 denotes itr = b1 - (b2 - b1)
+
+// it seems like a iterate process
+
+// so I initial itr = b1
+// then do the following process
+// if b1 > (b2 - itr)
+// then we can get b1 - (b2 - itr) and b1 - (b2 - itr) + b2 i.e. b1 + itr
+// else b1 <= b2 - itr
+// then we can get b1 + itr, as b1 can not fill b2 with itr
+// continue this process to find all the num we can get, if once itr repeat again then
+// we can stop this process
+
+// okay, speed is far too slow
+// okay, view the discuss, omg, it is a GCD problem -_-
+
 class Solution {
 public:
     bool canMeasureWater(int x, int y, int z) {
