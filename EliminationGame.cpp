8f0d1f5dbedd09d9e198b84b7dfaca4fed3b1def
zhesi.hzs <zhesih@163.com>
2016-10-03 04:07:39 +0100

    tricky

--- EliminationGame.cpp
+++ EliminationGame.cpp
@@ -1,40 +1,33 @@
-// 模拟的思路
-// Time O(logN)
-// Space O(n)
+// 二分的思路
+// 实际上并不用维护每个数字
+// 维护序列的起始数字 start index
+// 及序列的间隔即可 step
 
-// Memory Limit Exceeded
+// 当剩余序列的长度为奇数时 forward 与 backward 结果相同
+// 为偶数时
+
+// 当剩余序列长度为 1 时, 退出
 
 class Solution {
 public:
 	int lastRemaining(int n) {
-		vector<int> nums(n, 0);
-		for (int i = 0; i < n; ++i) {
-			nums[i] = i + 1;
-		}
-
-		int remain = n;
 		int start = 0;
+		int step = 1;
 		bool forward = true;
+		int remain = n;
 		
 		while (remain > 1) {
-			int t = n - 1;
-			int i = t - 1;
-			if (!(remain & 1) && forward) { // even and forward
-				--t;
-				--i;
+			if (remain & 1 || forward) {
+				start += step;
+				step <<= 1;
 			}
-
-			while (i >= start) {
-				swap(nums[i], nums[t--]);
-				i -= 2;
+			else {
+				step <<= 1;
 			}
-
-			remain -= (t - start + 1);
-			start = t + 1;
-
+			remain >>= 1;
 			forward = !forward;
 		}
 		
-		return nums[n - 1];
+		return start + 1;
 	}
 };
