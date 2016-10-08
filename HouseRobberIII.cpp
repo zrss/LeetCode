1bd8bc8d9cf8dffab2f7a9c60fe0b47962196d0b
zhesi.hzs <zhesih@163.com>
2016-10-08 15:25:28 +0100

    up to bottom

--- HouseRobberIII.cpp
+++ HouseRobberIII.cpp
@@ -1,26 +1,38 @@
-// 自底向上
+// 自顶向下
+// 记忆化搜索
 
 class Solution {
 public:
 	int rob(TreeNode* root) {
-		pair<int, int> p = solve(root);
-		return max(p.first, p.second);
+		return max(solve(root, true), solve(root, false));
 	}
 
-	// <robMaxValue, unRobMaxValue>
-	pair<int, int> solve(TreeNode* root) {
+	int solve(TreeNode* root, bool rob) {
 		if (root == NULL)
-			return make_pair(0, 0);
+			return 0;
 
-		pair<int, int> left = solve(root->left);
-		pair<int, int> right = solve(root->right);
+		if (rob) {
+			unordered_map<TreeNode*, int>::iterator itr = robValue.find(root);
+			if (itr != robValue.end()) {
+				return itr->second;
+			}
+			int value = root->val + solve(root->left, false) + solve(root->right, false);
+			robValue[root] = value;
+			return value;
+		}
 
-		// rob
-		int robValue = root->val + left.second + right.second;
+		unordered_map<TreeNode*, int>::iterator itr = unRobValue.find(root);
+		if (itr != robValue.end()) {
+			return itr->second;
+		}
 
-		// unrob
-		int unRobValue = max(left.first, left.second) + max(right.first, right.second);
-
-		return make_pair(robValue, unRobValue);
+		int t1 = max(solve(root->left, true), solve(root->left, false));
+		int t2 = max(solve(root->right, true), solve(root->right, false));
+		unRobValue[root] = t1 + t2;
+		return t1 + t2;
 	}
+
+private:
+	unordered_map<TreeNode*, int> robValue;
+	unordered_map<TreeNode*, int> unRobValue;
 };
