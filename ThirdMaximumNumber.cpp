70bd80719baca1f78443f76dffc128d94b7b9aad
Zhesi.Huang <zrss@ZhesiHuangdeMacBook-Pro.local>
2016-10-20 23:00:15 +0800

    smart ring

--- ThirdMaximumNumber.cpp
+++ ThirdMaximumNumber.cpp
@@ -2,31 +2,36 @@
 // 用了 set
 // 和 优先队列
 
+// 算了, 还是不偷懒了
+// 写个精细一点的
+
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        set<int> nset;
+        int maxNum = INT_MIN;
+        int midNum = INT_MIN;
+        int smallNum = INT_MIN;
+
+        int cnt = 0;
         int len = nums.size();
         for (int i = 0; i < len; ++i) {
-            if (nset.find(nums[i]) == nset.end()) {
-                if (pq.size() < 3) {
-                    pq.push(nums[i]);
-                    nset.insert(nums[i]);
-                } else {
-                    if (nums[i] > pq.top()) {
-                        nset.erase(pq.top());
-                        pq.pop();
-                        pq.push(nums[i]);
-                        nset.insert(nums[i]);
-                    }
-                }
+            if (nums[i] > maxNum) {
+                smallNum = midNum;
+                midNum = maxNum;
+                maxNum = nums[i];
+                ++cnt;
+            } else if (nums[i] != maxNum && nums[i] > midNum) {
+                smallNum = midNum;
+                midNum = nums[i];
+                ++cnt;
+            } else if (nums[i] != midNum && nums[i] > smallNum) {
+                smallNum = nums[i];
+                ++cnt;
             }
         }
-        if (pq.size() == 3)
-            return pq.top();
-        if (pq.size() == 2)
-            pq.pop();
-        return pq.top();
+
+        if (cnt >= 3)
+            return smallNum;
+        return maxNum;
     }
 };
