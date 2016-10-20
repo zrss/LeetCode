605ea218ed801f4c5bebaf0ef991e71c53c28649
Zhesi.Huang <zrss@ZhesiHuangdeMacBook-Pro.local>
2016-10-20 23:53:16 +0800

    more detail

--- ThirdMaximumNumber.cpp
+++ ThirdMaximumNumber.cpp
@@ -5,33 +5,30 @@
 // 算了, 还是不偷懒了
 // 写个精细一点的
 
+// 边界条件有点恶心
+// 直接用了 LLI
+
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        int maxNum = INT_MIN;
-        int midNum = INT_MIN;
-        int smallNum = INT_MIN;
+        long long int maxNum = LLONG_MIN;
+        long long int midNum = LLONG_MIN;
+        long long int smallNum = LLONG_MIN;
 
-        int cnt = 0;
         int len = nums.size();
         for (int i = 0; i < len; ++i) {
             if (nums[i] > maxNum) {
                 smallNum = midNum;
                 midNum = maxNum;
                 maxNum = nums[i];
-                ++cnt;
-            } else if (nums[i] != maxNum && nums[i] > midNum) {
+            } else if (nums[i] < maxNum && nums[i] > midNum) {
                 smallNum = midNum;
                 midNum = nums[i];
-                ++cnt;
-            } else if (nums[i] != midNum && nums[i] > smallNum) {
+            } else if (nums[i] < midNum && nums[i] > smallNum) {
                 smallNum = nums[i];
-                ++cnt;
             }
         }
 
-        if (cnt >= 3)
-            return smallNum;
-        return maxNum;
+        return smallNum != LLONG_MIN ? smallNum : maxNum;
     }
 };
